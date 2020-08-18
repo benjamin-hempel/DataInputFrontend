da4c29777b83b362b34e661fc74d2b19ca14c316
Benjamin Hempel <benjamin.hempel@saxsys.de>
2020-08-18 10:50:11 +0200

    Add TimeRepo to store times

---
+++ DataInputt/DataInputt/TimeRepo.cs
@@ -0,0 +1,21 @@
+﻿using DataInputt.DataInputServiceReference;
+using DataInputt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataInputt
+{
+    static class TimeRepo
+    {
+        public static List<Time> Times { get; set; }
+        public delegate void TimesCollectionImportedEventHandler(object sender, EventArgs e);
+        public static event TimesCollectionImportedEventHandler TimesCollectionImported;
+        public static void OnTimesCollectionImport()
+        {
+            TimesCollectionImported(TimeRepo.Times, new EventArgs());
+        }
+    }
+}
